dd18f63eb88271f1d41fbe3b9c03c9b65a169a69
kickscar <kickscar@gmail.com>
2021-08-17 13:55:31 +0900

    default nesting

--- 03.Routing/src/ex05/component/about/About.js
+++ 03.Routing/src/ex05/component/about/About.js
@@ -9,7 +9,6 @@ export default function About() {
     return (
         <SiteLayout>
             <Route component={Navigation}/>
-            <Route exact path='/about' component={Me}/>
             <Route path='/about/kickscar' component={Me}/>
             <Route path='/about/location' component={Location}/>
         </SiteLayout>
