6b65b50c146f7fb49fabb94f309a386141e6583a
kickscar <kickscar@gmail.com>
2022-03-27 15:41:39 +0900

    msa refactoring #3

--- emaillist8/backend/src/main/java/com/example/demo/controller/ApiController.java
+++ emaillist8/backend/src/main/java/com/example/demo/controller/ApiController.java
@@ -1,21 +1,31 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.consumer.EmaillistConsumer;
 import com.example.demo.dto.JsonResult;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 // @CrossOrigin(origins = {"http://localhost:9999"}, allowedHeaders = "*", allowCredentials="false", methods={RequestMethod.GET})
-public class ApiController {		
+public class ApiController {
+	
+	@Autowired
+	private EmaillistConsumer emaillistConsumer;
+	
 	@GetMapping("/api")
 	public ResponseEntity<JsonResult> read(@RequestParam(value="kw", required=true, defaultValue="") String keyword) {
+		
 		return ResponseEntity
 				.status(HttpStatus.OK)
-				.body(JsonResult.success(null));
+				.body(JsonResult.success(emaillistConsumer.getAllEmail()));
 	}
 	
 //	@PostMapping("/api")
---
+++ emaillist8/backend/src/main/java/com/example/demo/vo/EmaillistVo.java
@@ -0,0 +1,18 @@
+package com.example.demo.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+//@EqualsAndHashCode
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class EmaillistVo {
+	private Long no;
+	private String firstName;
+	private String lastName;
+	private String email;	
+}
