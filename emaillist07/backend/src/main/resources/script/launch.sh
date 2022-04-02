78eff32bcdfd128b039379ece004de30fd5e0cde
kickscar <kickscar@gmail.com>
2022-04-02 17:45:49 +0900

    msa #8

--- emaillist07/backend/src/main/resources/script/launch.sh
+++ emaillist07/backend/src/main/resources/script/launch.sh
@@ -7,10 +7,10 @@ PID=$(ps -ef | grep java | grep $SERVICE_NAME.jar | awk '{print $2}')
 
 if  [ ! -z "$PID" ] 
 then
-	echo "stopping [$SERVICE_NAME][$JAVA]" > $SCRIPT_DIR/test.txt
+	echo "stopping [$SERVICE_NAME]"
 	kill -9 $PID
 	sleep 10
 fi
 
 echo "starting [$SERVICE_NAME]"
-nohup $JAVA -Dspring.profiles.active=production -jar $SCRIPT_DIR/$SERVICE_NAME.jar >> $SCRIPT_DIR/launch.log &
+nohup java -Dspring.profiles.active=production -jar $SCRIPT_DIR/$SERVICE_NAME.jar >> $SCRIPT_DIR/launch.log &
